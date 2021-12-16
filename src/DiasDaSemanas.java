a18b36f4d612c68c91069ee1cd494bc496ac0115
guibassgui <85911756+guibassgui@users.noreply.github.com>
2021-12-16 13:35:11 -0300

    Exercicio_em_java_dias_da_semana.git

--- src/DiasDaSemanas.java
+++ src/DiasDaSemanas.java
@@ -1,9 +1,51 @@
+import java.util.Scanner;
 
 public class DiasDaSemanas {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 
+		Integer x = sc.nextInt();
+		  String dia;
+		  
+		  if (x == 1) {
+			  dia = "domingo";
+		  } else if(x == 2 ) {
+			  dia = "segunda";
+		  } else if (x == 3) {
+			  dia = "terça";
+		  } else if (x == 4 ) {
+			  dia = "quarta";
+		  } else {
+			  dia = "Valor invalido";
+		  }
 		
-
+        //  System.out.println("Dia da semana:" + dia);
+		
+		
+		// Switch Case
+		
+		Integer x = sc.nextInt();
+		String dia;
+		    
+		    switch (x) {
+			case 1:
+				dia = "domingo";
+				break;
+			case 2:
+			    dia = "senguda";
+			    break;
+			default:
+				dia = "valor invalido";
+				break;
+			}
+		
+		 System.out.println("Dia da semana:" +   dia );
+		         
+           sc.close();
 	}
 
 }
+
+
+	
